1dd6af177ccfd049ccf507bf8fe7afd3a2b77c1e
Dan Needham <dneedham@gmail.com>
2017-02-07 17:30:11 +0000

    Project config

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,4 @@
 const path = require('path');
-const webpack = require('webpack');
-const fs = require('fs');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
@@ -15,31 +13,31 @@ module.exports = {
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js',
-    publicPath: '/'
+    publicPath: '/',
   },
   module: {
     loaders: [
       {
         test: /\.css$/,
         exclude: /node_modules/,
-        loader: 'style-loader!css-loader!postcss-loader'
+        loader: 'style-loader!css-loader!postcss-loader',
       },
       {
         test: /\.js$/,
         loader: require.resolve('babel-loader'),
-        exclude: /node_modules/
-      }
-    ]
+        exclude: /node_modules/,
+      },
+    ],
   },
   plugins: [
     new ExtractTextPlugin('style.css'),
-    new CopyWebpackPlugin([ { from: 'public/index.html', to: 'index.html' } ])
+    new CopyWebpackPlugin([ { from: 'public/index.html', to: 'index.html' } ]),
   ],
   postcss: function () {
     return [
       require('precss'),
-      require('autoprefixer')
+      require('autoprefixer'),
     ];
-  }
+  },
 };
 
