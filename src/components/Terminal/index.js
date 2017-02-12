08e0bbac4615185c480ebef11dd621b0eafec362
Dan Needham <dneedham@gmail.com>
2017-02-12 15:03:07 +0000

    preventDefault on non alpha-numeric input

--- src/components/Terminal/index.js
+++ src/components/Terminal/index.js
@@ -7,6 +7,21 @@ class Input extends Component {
     value: PropTypes.string,
   }
 
+  componentDidMount () {
+    document.getElementById('input').addEventListener('keypress', this.handleKeypress);
+  }
+
+  componentWillUnmount () {
+    document.getElementById('input').removeEventListener('keypress');
+  }
+
+  handleKeypress = (event) => {
+    const pattern = new RegExp(/^[a-z0-9]+$/i);
+    if (!pattern.test(event.key)) {
+      event.preventDefault();
+    }
+  }
+
   focus = element => element.focus();
 
   render () {
